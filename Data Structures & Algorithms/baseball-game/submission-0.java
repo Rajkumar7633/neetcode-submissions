d53d8bec3543cee530908524361451c8676b8509
Raj Kumar <118206888+Rajkumar7633@users.noreply.github.com>
2026-07-25 13:32:18 +0530

    Add: baseball-game - submission-0

---
+++ Data Structures & Algorithms/baseball-game/submission-0.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        Stack<Integer> st = new Stack<>();
+        for(String op : operations){
+            if(op.equals("+")){
+                int first = st.pop();
+                int second = st.peek();
+                st.push(first);
+                st.push(first+second);
+            }
+            else if (op.equals("D")){
+                st.push(st.peek() * 2);
+            }
+            else if(op.equals("C")){
+                st.pop();
+            }
+            else{
+                st.push(Integer.parseInt(op));
+            }
+        }
+        int sum =0;
+        while(!st.isEmpty()){
+            sum += st.pop();
+        }
+        return sum;
+    }
+}
